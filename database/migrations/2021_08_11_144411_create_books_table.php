906da5791e22b44c5d6905e570ea0afeac84fd54
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2021-08-12 00:35:15 +0900

    ルーティングにすべての処理をまとめて書いた

---
+++ database/migrations/2021_08_11_144411_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name'); //書籍名
+            $table->integer('item_number'); //冊数
+            $table->integer('item_amount'); //金額
+            $table->datetime('published'); //出版日
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Book;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,76 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+/**
+ * 本の一覧
+ */
 Route::get('/', function () {
-    return view('welcome');
+    $books = Book::orderBy('created_at', 'asc')->get();
+    return view('books',[
+        'books' => $books
+    ]);
 });
+
+
+/**
+* 本を追加 
+*/
+Route::post('/books', function (Request $request) {
+    
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|max:255',
+    ]);
+    //バリデーションエラー
+    if($validator->fails()){
+        return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+    }
+    
+    //Eloquentモデル(登録処理)
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount =  $request->item_amount;
+    $books->published =  $request->published;
+    $books->save();
+    return redirect('/');
+});
+
+/**
+* 本を編集
+*/
+Route::post('/bookedit/{book}', function (Request $request) {
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|max:255',
+    ]);
+    //バリデーションエラー
+    if($validator->fails()){
+        return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+    }
+    
+    //Eloquentモデル(登録処理)
+    $books = Book::find($request->id);
+    $books->item_name   = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published   = $request->published;
+    $books->save();
+    return redirect('/');
+});
+
+
+/**
+* 本を削除 
+*/
+Route::delete('/books/{book}', function (Book $book) {
+    $book->delete();
+    return redirect('/');
+});
+
