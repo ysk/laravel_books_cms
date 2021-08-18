b05cc9770081f3b41d79d2e235612a20e37ded9e
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2021-08-18 23:47:26 +0900

    インデント整える

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -14,25 +14,19 @@ class MypageController extends Controller
 
     public function show()
     {
-        $id   = Auth::id();
-        $user = User::find($id);
-
-        //マイページで自分が登録した書籍のみ表示するときは下記
-        $books = Book::where('user_id', Auth::user()->id)->orderBy('created_at', 'asc')->paginate(5);
-
-
+        $id    = Auth::id();
+        $user  = User::find($id);
+        $books = Book::where('user_id', $id)->orderBy('created_at', 'asc')->paginate(5);
         return view('mypage.profileShow',[
-            'user' => $user,
+            'user'  => $user,
             'books' => $books,
         ]);
     }
 
     public function edit()
     {
-
         $id   = Auth::id();
         $user = User::find($id);
-
         return view('mypage.profileEdit',[
             'user' => $user,
         ]);
@@ -41,9 +35,9 @@ class MypageController extends Controller
     public function update(profileUpdateRequest $request, User $user) {
         $id   = Auth::id();
         $user = User::find($id);
-        $user->name   = $request->name;
+        $user->name  = $request->name;
         $user->email = $request->email;
-        $user->text = $request->text;
+        $user->text  = $request->text;
         $user->url   = $request->url;
         $user->save();
         return redirect('/mypage/show')->with('message','書籍を更新しました');
